c1caf7e5fca342c11a55a974860cd080adeb26d0
axelsrz <axel.srz@gmail.com>
2015-10-28 12:21:46 -0600

    cambio de subjuegos a paneles

--- src/pizzagaiden/Dimensiones.java
+++ src/pizzagaiden/Dimensiones.java
@@ -10,5 +10,9 @@ package pizzagaiden;
  * @author axelsuarez
  */
 public interface Dimensiones {
-    
+    public static final int WIDTH = 1000;    //ancho
+    public static final int HEIGTH = 700;   //largo
+    public static final int BOTTOM = 580;   //Si llega aqui se pierde una vida
+    public static final int PLATAFORMA_RIGHT = 410; //Hasta donde puede llegar la plataforma
+    public static final int bola_RIGHT = 465;   //Hasta donde puede llegar la plataforma
 }
