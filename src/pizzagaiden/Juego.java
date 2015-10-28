c1d733bdc3ed5fe14208587c08fd45695f5bf3e0
Evan Juarez <e.juav24@gmail.com>
2015-10-28 11:52:56 -0600

    Crea las clases principales de los juegos que van a ser cargados dentro del JFrame

--- src/pizzagaiden/Juego.java
+++ src/pizzagaiden/Juego.java
@@ -5,12 +5,41 @@
  */
 package pizzagaiden;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 /**
  *
  * @author Evan
  */
-public class Juego extends JFrame {
+public class Juego extends JFrame implements ActionListener, KeyListener, Runnable {
     
+   
+    
+    void init() {
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+    }
+
+    @Override
+    public void run() {
+    }
 }
